1340e14ad6299a434ad5b06564802ee552efaf7f
Gerard Sala <gerard.sala@outlook.com>
2016-07-21 16:43:49 +0200

    Add Tab Navigation between fragments

--- app/src/main/java/edu/upc/mcia/publications/ui/MainActivity.java
+++ app/src/main/java/edu/upc/mcia/publications/ui/MainActivity.java
@@ -1,15 +1,79 @@
 package edu.upc.mcia.publications.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import edu.upc.mcia.publications.R;
+import edu.upc.mcia.publications.ui.authors.AuthorsFragment;
+import edu.upc.mcia.publications.ui.publishers.PublishersFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ViewPager mViewPager;
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setOffscreenPageLimit(2);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+        mViewPager.setCurrentItem(1, false);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new AuthorsFragment();
+                case 1:
+                    return new Fragment();
+                case 2:
+                    return new PublishersFragment();
+                default:
+                    return new Fragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Authors";
+                case 1:
+                    return "Publications";
+                case 2:
+                    return "Publishers";
+                default:
+                    return "";
+            }
+        }
     }
 }
