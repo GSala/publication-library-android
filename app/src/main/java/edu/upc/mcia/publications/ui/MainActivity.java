75bc0558aee978b0b0ee1beb6552ac82df1ff97a
Gerard Sala <gerard.sala@outlook.com>
2016-08-02 17:31:33 +0200

    Swith PagerAdapter implementation
    FragmentPagerAdapter is the correct implementation to use when using fixed static tabs

--- app/src/main/java/edu/upc/mcia/publications/ui/MainActivity.java
+++ app/src/main/java/edu/upc/mcia/publications/ui/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
     }
 
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+    private class ScreenSlidePagerAdapter extends FragmentPagerAdapter {
         public ScreenSlidePagerAdapter(FragmentManager fm) {
             super(fm);
         }
